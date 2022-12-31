358af45e4c16012152c6316c604478545b8617ad
werniq <qniwwwersss@gmail.com>
2022-12-31 13:43:28 +0100

    rd

---
+++ go.mod
@@ -0,0 +1,11 @@
+module ds-bot
+
+go 1.19
+
+require (
+	github.com/bwmarrin/discordgo v0.24.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+type GifSearch struct {
+	Data struct {
+		Type             string `json:"type"`
+		Id               string `json:"id"`
+		Url              string `json:"url"`
+		Slug             string `json:"slug"`
+		BitlyGifUrl      string `json:"bitly_gif_url"`
+		BitlyUrl         string `json:"bitly_url"`
+		EmbedUrl         string `json:"embed_url"`
+		Username         string `json:"username"`
+		Source           string `json:"source"`
+		Title            string `json:"title"`
+		Rating           string `json:"rating"`
+		ContentUrl       string `json:"content_url"`
+		SourceTld        string `json:"source_tld"`
+		SourcePostUrl    string `json:"source_post_url"`
+		IsSticker        int    `json:"is_sticker"`
+		ImportDatetime   string `json:"import_datetime"`
+		TrendingDatetime string `json:"trending_datetime"`
+		Images           struct {
+			FixedWidthStill struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"fixed_width_still"`
+			PreviewGif struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"preview_gif"`
+			FixedHeightDownsampled struct {
+				Height   string `json:"height"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"fixed_height_downsampled"`
+			Preview struct {
+				Height  string `json:"height"`
+				Mp4     string `json:"mp4"`
+				Mp4Size string `json:"mp4_size"`
+				Width   string `json:"width"`
+			} `json:"preview"`
+			FixedHeightSmall struct {
+				Height   string `json:"height"`
+				Mp4      string `json:"mp4"`
+				Mp4Size  string `json:"mp4_size"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"fixed_height_small"`
+			Downsized struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"downsized"`
+			FixedWidthDownsampled struct {
+				Height   string `json:"height"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"fixed_width_downsampled"`
+			FixedWidth struct {
+				Height   string `json:"height"`
+				Mp4      string `json:"mp4"`
+				Mp4Size  string `json:"mp4_size"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"fixed_width"`
+			DownsizedStill struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"downsized_still"`
+			DownsizedMedium struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"downsized_medium"`
+			OriginalMp4 struct {
+				Height  string `json:"height"`
+				Mp4     string `json:"mp4"`
+				Mp4Size string `json:"mp4_size"`
+				Width   string `json:"width"`
+			} `json:"original_mp4"`
+			DownsizedLarge struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"downsized_large"`
+			PreviewWebp struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"preview_webp"`
+			Original struct {
+				Frames   string `json:"frames"`
+				Hash     string `json:"hash"`
+				Height   string `json:"height"`
+				Mp4      string `json:"mp4"`
+				Mp4Size  string `json:"mp4_size"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"original"`
+			OriginalStill struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"original_still"`
+			FixedHeightSmallStill struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"fixed_height_small_still"`
+			FixedWidthSmall struct {
+				Height   string `json:"height"`
+				Mp4      string `json:"mp4"`
+				Mp4Size  string `json:"mp4_size"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"fixed_width_small"`
+			Looping struct {
+				Mp4     string `json:"mp4"`
+				Mp4Size string `json:"mp4_size"`
+			} `json:"looping"`
+			DownsizedSmall struct {
+				Height  string `json:"height"`
+				Mp4     string `json:"mp4"`
+				Mp4Size string `json:"mp4_size"`
+				Width   string `json:"width"`
+			} `json:"downsized_small"`
+			FixedWidthSmallStill struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"fixed_width_small_still"`
+			FixedHeightStill struct {
+				Height string `json:"height"`
+				Size   string `json:"size"`
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+			} `json:"fixed_height_still"`
+			FixedHeight struct {
+				Height   string `json:"height"`
+				Mp4      string `json:"mp4"`
+				Mp4Size  string `json:"mp4_size"`
+				Size     string `json:"size"`
+				Url      string `json:"url"`
+				Webp     string `json:"webp"`
+				WebpSize string `json:"webp_size"`
+				Width    string `json:"width"`
+			} `json:"fixed_height"`
+			WStill struct {
+				Url    string `json:"url"`
+				Width  string `json:"width"`
+				Height string `json:"height"`
+			} `json:"480w_still"`
+		} `json:"images"`
+	} `json:"data"`
+	Meta struct {
+		Msg        string `json:"msg"`
+		Status     int    `json:"status"`
+		ResponseId string `json:"response_id"`
+	} `json:"meta"`
+}
+
+const botPrefix string = "$"
+
+func main() {
+	// 1
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	Token := os.Getenv("DISCORD_TOKEN")
+
+	// 2
+	bot, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("Error creating a discord Session, ", err)
+	}
+
+	// 3
+	bot.AddHandler(ready)
+	bot.AddHandler(messageCreate)
+	// bot.Identify.Intents = discordgo.IntentsGuildMessages
+	// 4
+	err = bot.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord Session, ", err)
+	}
+	fmt.Println("The bot is now running. Press CTRL-C to exit.")
+
+	// 5
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+func ready(s *discordgo.Session, event *discordgo.Event) {
+	s.UpdateGameStatus(0, botPrefix + "search < keyword >")
+}
+
+func messageCreate(s *discordgo.Session, message *discordgo.MessageCreate) {
+ 
+	// 1
+	err := godotenv.Load(".env")
+	giphyToken := os.Getenv("GIPHY_TOKEN")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 2
+	if message.Author.ID == s.State.User.ID {
+		return
+	}
+	// 3
+	command := strings.Split(message.Content, " ")
+	// 4
+	if command[0] == "$search" && len(command) > 1 {
+		url := "https://api.giphy.com/v1/gifs/random"
+		var result GifSearch
+		// 5
+		gifKeyword := strings.Join(command[1:], " ")
+		// 6
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Println("Error in making a new Request", err)
+		}
+		query := req.URL.Query()
+		query.Add("api_key", giphyToken)
+		query.Add("tag", gifKeyword)
+		req.URL.RawQuery = query.Encode()
+		client := http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error in getting a response, ", err)
+		}
+		body, _ := ioutil.ReadAll(res.Body)
+		if err := json.Unmarshal(body, &result); err != nil {
+			fmt.Println("Can not unmarshall JSON", err)
+		}
+		// 7
+		s.ChannelMessageSend(message.ChannelID, result.Data.EmbedUrl)
+		res.Body.Close()
+	}
+}
+
+
+// func say(s *discordgo.Session, mes *discordgo.MessageCreate) {
+// 	if mes.Author.ID == s.State.User.ID {
+// 		return
+// 	}
+// 	command := strings.Split(mes.Content, " ")
+	
+// 	if command[0] == botPrefix + "say" && len(mes) > 1 {
+// 		// messageToSend := strings.Join()
+// 		s.ChannelMessageSend(mes.ChannelID, )
+// 	}
+// }
