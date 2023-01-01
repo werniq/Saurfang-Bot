975e0527a0addf76a07033b14dc9ef0ac33a6275
werniq <qniwwwersss@gmail.com>
2023-01-01 15:35:14 +0100

    Updated README.md and added some new functions to bot

---
+++ README.md
@@ -0,0 +1,20 @@
+> Discord bot 
+<hr>
+<h3>Discord bot's become quite popular project, to practice golang. So I decided to make my own bot too <3</h3>
+## This bot has next functionalities:
+- func searchGifs
+    - This function you can call by typing $search <-keyword->
+    - It sends you back a gif, which represents given keywoard
+- func changeStatus
+    - changeStatus function you can't call.
+    - All functionality, that this function executes on running, is just changing the bot's status.
+- func help
+    - Help function you can call by typing $help
+    - Help function sends you message, with all functions that this bot can run.
+- func greetNewMember
+    - greetNewMember function you can't call. However, it is executed whenever new user joins guild.
+    - It just prints greeting, suitable to my server
+- func listRules
+    - listRules function called, any time you type $listRules 
+    - This function just returns some rules, that I declared at the reasons map
+- reasons map used for banning users with reason    
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -205,7 +206,41 @@ type GifSearch struct {
 	} `json:"meta"`
 }
 
-const botPrefix string = "$"
+const (
+	botPrefix = "$"
+	ownerChatId 	 = "1058658554325770320"
+	forTheHordeLink = "https://media.giphy.com/media/xThtatRttFzLD9oEtG/giphy.gif"
+	message = `
+	Hey!
+	My name is Saurfang #or Super Bebra # , and I am extremely useful bot!
+	I have such commands: 
+		$search {keyword} 	  - Send gif, which represents keyword
+		$help 			      - List of commands, which this bot wields
+		$ban --replyToMessage - Bans author of replied message
+		$listReasons		  - Prints all rules, in this server
+
+	`
+)
+
+var (
+	reasons = map[int]string{
+		1:		"Harass, bully, or threat marginalized or vulnerable groups of people",
+		2:		"Engage in content manipulation (spamming, subscriber fraud, vote manipulation, or ban evasion)",
+		3:		"Post or threaten to post intimate or sexually explicit photos or videos of another person without their consent",
+		4:		"Impersonate someone in a misleading way",
+		5:		"Label the content and communities improperly (especially graphic content)",
+		6: 		"Post suggestive or sexual content that involves minors",
+		7: 		"Post illegal content",
+		8: 		"Do anything that stops the normal use of this server",
+		9: 		"Transmit, distribute, or upload any viruses, worms, or other malware intended to interfere with this servers service",
+		10: 	"Use the platform to violate the law or infringe on intellectual and other property right",
+		11: 	"Engage in actions that could disrupt, disable, overburden, or impair this servers service",
+		12: 	"Attempt to gain access to another user`s account",
+		13: 	"Access, search, or collect data from Reddit",
+		14: 	"Use the platform in any way that may be abusive or fraudulent",
+	}	
+)
+
 
 func main() {
 	// 1
@@ -221,11 +256,20 @@ func main() {
 		fmt.Println("Error creating a discord Session, ", err)
 	}
 
-	// 3
-	bot.AddHandler(ready)
-	bot.AddHandler(messageCreate)
-	// bot.Identify.Intents = discordgo.IntentsGuildMessages
-	// 4
+	// set status
+	bot.AddHandler(changeStatus)
+	// list all commands
+	bot.AddHandler(help)
+	// send gif, which represents keyword
+	bot.AddHandler(searchGifs)
+	// bans user
+	bot.AddHandler(ban)
+	// say greetengs to new member
+	bot.AddHandler(greetNewMember)
+	// lists all reasons, using which user can be banned
+	bot.AddHandler(listRules)
+	// bot.AddHandler(thisServerIsAlwaysAlive)
+
 	err = bot.Open()
 	if err != nil {
 		fmt.Println("Error opening Discord Session, ", err)
@@ -238,41 +282,44 @@ func main() {
 	<-sc
 }
 
-func ready(s *discordgo.Session, event *discordgo.Event) {
-	s.UpdateGameStatus(0, botPrefix + "search < keyword >")
+func changeStatus(s *discordgo.Session, event *discordgo.Event) {
+	fmt.Println("ready function running")
+	s.UpdateGameStatus(1, "I Love Hot Milfs")
+	// s.UpdateGameStatus()
+	
 }
 
-func messageCreate(s *discordgo.Session, message *discordgo.MessageCreate) {
- 
+func searchGifs(s *discordgo.Session, message *discordgo.MessageCreate) {
+	fmt.Println("Gif function running")
 	// 1
 	err := godotenv.Load(".env")
 	giphyToken := os.Getenv("GIPHY_TOKEN")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// 2
-	if message.Author.ID == s.State.User.ID {
-		return
-	}
-	// 3
+	
+
 	command := strings.Split(message.Content, " ")
-	// 4
-	if command[0] == "$search" && len(command) > 1 {
+	
+	if command[0] == botPrefix + "search" && len(command) > 1 {
 		url := "https://api.giphy.com/v1/gifs/random"
 		var result GifSearch
-		// 5
+		
 		gifKeyword := strings.Join(command[1:], " ")
-		// 6
+		
 		req, err := http.NewRequest("GET", url, nil)
+
 		if err != nil {
 			fmt.Println("Error in making a new Request", err)
 		}
+
 		query := req.URL.Query()
 		query.Add("api_key", giphyToken)
 		query.Add("tag", gifKeyword)
 		req.URL.RawQuery = query.Encode()
 		client := http.Client{}
 		res, err := client.Do(req)
+
 		if err != nil {
 			fmt.Println("Error in getting a response, ", err)
 		}
@@ -280,7 +327,7 @@ func messageCreate(s *discordgo.Session, message *discordgo.MessageCreate) {
 		if err := json.Unmarshal(body, &result); err != nil {
 			fmt.Println("Can not unmarshall JSON", err)
 		}
-		// 7
+		
 		s.ChannelMessageSend(message.ChannelID, result.Data.EmbedUrl)
 		res.Body.Close()
 	}
@@ -296,5 +343,75 @@ func messageCreate(s *discordgo.Session, message *discordgo.MessageCreate) {
 // 	if command[0] == botPrefix + "say" && len(mes) > 1 {
 // 		// messageToSend := strings.Join()
 // 		s.ChannelMessageSend(mes.ChannelID, )
-// 	}
+// 	}asdfadf
 // }
+func help(s *discordgo.Session, mes *discordgo.MessageCreate) {
+	// command := strings.Split(m.Content, " ")
+	if mes.Content == botPrefix + "help" {
+		s.ChannelMessageSend(mes.ChannelID, message)
+	}
+}
+
+func ban(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println("Ban function running")
+	
+	refMes := m.ReferencedMessage
+	command := strings.Split(m.Content, " ")
+	// botPRefix + ban + "1-14"
+	if command[0] == botPrefix + "ban" && len(command) > 1 {	
+		if m.Content == botPrefix + "ban " + command[1] {
+			banReason, _ := strconv.Atoi(command[1])
+			err := s.GuildBanCreateWithReason(m.GuildID, refMes.Author.ID, reasons[banReason], 7)
+			
+			if err != nil {
+				s.ChannelMessageSend(m.ChannelID, "Something went wrong.. Reported to owner")
+				mes :=  fmt.Sprintf("Error banning user %s | in chat %s.", ownerChatId, m.ChannelID)
+				s.ChannelMessageSend(ownerChatId, mes)
+			}
+			s.ChannelMessageSend(m.ChannelID, "User is banned")
+			return
+		} 
+		s.ChannelMessageSend(m.ChannelID, "Please, provide a reason.")	 
+	}	
+}
+
+func listRules(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var reason string
+	// command := strings.Split(m.Content, " ")
+	if m.Content == botPrefix + "listReasons"  {
+		for i := 1; i <= 14; i++ {
+			// i = strconv.Atoi(i)
+			reason = reasons[i]
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Reason %d: ----> %s", i, reason))
+		}
+	}
+}
+// func unban(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+// }
+
+func greetNewMember(s *discordgo.Session, mes *discordgo.MessageCreate) {
+	if mes.Type == 7 {
+		s.ChannelMessageSend(mes.ChannelID, `
+		I'm glad to see you here, recruit!
+		Here is some rules, you have to follow, to become a High Overload, Leader, or Warchief:
+			Do NOT:
+				Harass, bully, or threat marginalized or vulnerable groups of people,
+				Engage in content manipulation (spamming, subscriber fraud, vote manipulation, or ban evasion),
+				Post or threaten to post intimate or sexually explicit photos or videos of another person without their consent,
+				Impersonate someone in a misleading way,
+				Label the content and communities improperly (especially graphic content),
+		 		Post suggestive or sexual content that involves minors,
+		 		Post illegal content,
+		 		Do anything that stops the normal use of this server,
+		 		Transmit, distribute, or upload any viruses, worms, or other malware intended to interfere with this servers service,
+		     	Use the platform to violate the law or infringe on intellectual and other property right,
+		     	Engage in actions that could disrupt, disable, overburden, or impair this servers service,
+		     	Attempt to gain access to another user's account,
+		     	Access, search, or collect data from this server,
+		     	Use the platform in any way that may be abusive or fraudulent 
+		For The Horde!	
+	`)
+	s.ChannelMessageSend(mes.ChannelID, fmt.Sprint(forTheHordeLink))
+	}
+}
